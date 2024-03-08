bef789667362a6e362284b995ff38a286ccf71f8
Julio-orellana <84369228+Julio-orellana@users.noreply.github.com>
2024-03-08 14:03:51 -0600

    Actualización de Archivos 1
    Archivos con Funciones y variables principales definidas

--- EstructurasCiclicas2.py
+++ EstructurasCiclicas2.py
@@ -3,8 +3,23 @@ Integrantes: Marco Díaz & Julio Orellana
 Ejercicio Estructuras cíclicas
 Este proyecto creará una postal con una imagen en el lado izquierdo y un texto del lado derecho.
 """
-from modulos import rectangle
 
+#Importar Funciones de modulos
+from modulos import rectangle, square, triangle, mountain, ovalo, menu, lienzo
+#Importar funciones y la libreria turtle del modulo
 from modulos import turtle, t
 
+"""
+Instrucciones de uso de funciones:
+t.seth(-45)
+ovalo(80,3)
+t.seth(0)
+mountain(altura)
+rectangle(altura, base)
+square(altura, base)
+triangle(altura)
+ovalo(radio, angulo de curvatura)
+"""
+
 #Menu
+menu()
--- modulos.py
+++ modulos.py
@@ -1,5 +1,14 @@
+"""
+Integrantes: Marco Díaz & Julio Orellana
+Ejercicio Estructuras cíclicas
+Este proyecto creará una postal con una imagen en el lado izquierdo y un texto del lado derecho.
+"""
+#Importar Modulos
 import turtle
-t=turtle.turtle()
+#Definir t
+t = turtle.Turtle()
+#Asignar velocidad a t
+t.speed(20)
 
 """
 ● forward(distancia): avanza una determinada cantidad de píxeles, indicado en distancia.
@@ -15,16 +24,91 @@ t=turtle.turtle()
 """
 
 #Creacion de funciones del programa
+
+#Bibujar un rectangulo
 def rectangle(height,base):
-    a=0
-    while a==2:
-        t.turtle.setheading(0)
-        t.turtle.forward(base)
-        t.turtle.right(90)
-        t.turtle.forward(height)
-        a+=1
-def ovalo (height, base):
+    for r in range(2):
+        t.forward(base)
+        t.right(90)
+        t.forward(height)
+        t.right(90)
+#Dibujar un ovalo
+def ovalo (radio, angle):
+    for o in range(2):
+        t.circle(radio, 90)
+        t.circle(radio//angle, 90)
+#Dibujar un cuadrado
+def square (height, base):
+    for s in range(2):
+        t.forward(base)
+        t.right(90)
+        t.forward(height)
+        t.right(90)
+#Dibujar un triangulo
+def triangle (height):
+    t.left(60)
+    t.forward(height)
+    t.right(120)
+    t.forward(height)
+    t.right(120)
+    t.forward(height)
+#Dibujar una montaña
+def mountain(mRange, height):
+    mRange = 2 #Cuantas montañas necesitamos
+    for m in mRange:
+        t.left(60)
+        t.forward(height)
+        t.right(120)
+        t.forward(height)
+        t.left(60)
+#Menu Principal
+def menu ():
+    op = str(input("Desea enviar una carta?: s/n "))
+    while True:
+        if op == "s":
+            name1 = str(input("Ingrese el nombre del remitente: "))
+            name2 = str(input("Ingrese el nombre del destinatario: "))
+            lienzo()
+            #Prueba de dibujo y escritura en el nuevo lienzo de trabajo
+            t.penup()
+            t.goto(200, 150)
+            t.pendown()
+            t.write(name1)
+            t.write(name1)
+            t.penup()
+            t.goto(-200, 150)
+            t.pendown()
+            square(100, 100)
+            turtle.done()
+            op = str(input("Desea enviar otra carta?: s/n: "))
+        elif op == "n":
+            print("\nSaliendo...\n")
+            break
+        else: 
+            print("Error")
+            op = str(input("Desea enviar una carta?: s/n: "))
+#Linea que divide el lienzo
+def line (lenght):
+    t.left(90)
+    t.forward(lenght)
+    t.right(180)
+    t.forward(lenght*2)
+#Dibujar el nuevo lienzo en el que se trabajara
+def lienzo ():
+    t.width(5)
+    line(300)
+    t.left(90)
+    t.penup()
+    t.goto(-600, 300)
+    t.pendown()
+    rectangle(600, 1200)
 
-def circulo (height, base):
-
-def
+"""t.seth(-45)
+ovalo(80,3)
+t.seth(0)
+mountain(altura)
+rectangle(altura, base)
+square(altura, base)
+triangle(altura)
+ovalo(radio, angulo de curvatura)
+""" 
